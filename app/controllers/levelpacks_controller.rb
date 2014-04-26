58060ffeb3b08a1b21268966a89c0ae6b9dafde6
Steef Gijsbers <steef_gijsbers@hotmail.com>
2014-04-26 22:24:11 +0200

    Finished uploading, showing and indexing levelpacks

--- app/controllers/levelpacks_controller.rb
+++ app/controllers/levelpacks_controller.rb
@@ -1,10 +1,31 @@
 class LevelpacksController < ApplicationController
   
   def new
-    
+    @levelpack = Levelpack.new
   end
   
   def show
     @levelpack = Levelpack.find_by(id: params[:id])
   end
+  
+  def create
+    @levelpack = Levelpack.new(levelpack_params)
+    if @levelpack.save
+      flash[:success] = "Levelpack has been succesfully created."
+      redirect_to levelpacks_path
+    else
+      render 'new'
+    end
+  end
+  
+  def index
+    @levelpacks = Levelpack.paginate(page: params[:page])
+  end
+  
+  
+  private
+  
+    def levelpack_params
+      params.require(:levelpack).permit(:name, :title)
+    end
 end
--- spec/requests/levelpack_pages_spec.rb
+++ spec/requests/levelpack_pages_spec.rb
@@ -11,4 +11,50 @@ describe "Levelpack pages" do
     it { should have_content('Create levelpack') }
   end
   
+  describe "Show page" do
+    let(:levelpack) { FactoryGirl.create(:levelpack) }
+    before { visit levelpack_path(levelpack) }
+    
+    it { should have_title(full_title(levelpack.name)) }
+    it { should have_content levelpack.name }
+    it { should have_content levelpack.title }
+    it { should have_content levelpack.solution }
+  end
+  
+  describe "Index page" do
+    before do
+      visit new_levelpack_path
+      fill_in "Name: (format = 'levelpack_00')", with: "levelpack_00"
+      fill_in "Title: (max 50 char)",            with: "example levelpack"
+      click_button 'Create levelpack'
+      visit levelpacks_path
+    end    
+    
+    it { should have_selector('li', text: "levelpack_00") }
+    it { should have_title(full_title('Levelpack index')) }
+    it { should have_content 'Levelpack index' }
+    it { should have_link 'delete' }
+  end
+  
+  
+  describe "create levelpack" do
+    before { visit new_levelpack_path }    
+    let(:submit) { "Create levelpack" }
+    
+    describe "with invalid information" do
+      it "should not create a levelpack" do
+        expect { click_button submit }.not_to change(Levelpack, :count)
+      end
+    end
+    
+    describe "with valid information" do
+      before do
+        fill_in "Name: (format = 'levelpack_00')", with: "levelpack_00"
+        fill_in "Title: (max 50 char)",            with: "example levelpack"
+      end
+      it "should create a levelpack" do
+        expect { click_button submit }.to change(Levelpack, :count).by(1)
+      end
+    end
+  end
 end
