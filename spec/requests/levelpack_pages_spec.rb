6c7d0cdab1261b9415507deb4e1373c2d990a7d5
Stijn Meurkens <meurkens@gmail.com>
2014-04-29 18:46:36 +0200

    Adding levels to levelpacks.

--- spec/requests/levelpack_pages_spec.rb
+++ spec/requests/levelpack_pages_spec.rb
@@ -1,100 +1,123 @@
 require 'spec_helper'
 
 describe "Level pages" do
-  
+
   subject { page }
-  
+
   let(:user)      { FactoryGirl.create(:user) }
   let(:admin)     { FactoryGirl.create(:user, admin: true) }
   let(:levelpack) { FactoryGirl.create(:levelpack) }
-  
+
   describe "When non-admin user" do
     before { sign_in user, no_capybara: true }
-    
+
     describe "tries to access" do
       describe "upload page" do
-        before  { get new_levelpack_path }        
+        before  { get new_levelpack_path }
         specify { expect(response).to redirect_to root_url }
       end
-      
+
       describe "show page" do
         before  { get levelpack_path(levelpack) }
         specify { expect(response).to redirect_to root_url }
       end
-      
+
       describe "index page" do
         before  { get levelpacks_path }
         specify { expect(response).to redirect_to root_url }
-      end      
+      end
     end
-    
+
     describe "tries to upload a new level" do
       before  { post levelpacks_path(levelpack) }
       specify { expect(response).to redirect_to root_url }
     end
-    
+
     describe "tries to delete an existing level" do
       before  { delete levelpack_path(levelpack) }
       specify { expect(response).to redirect_to root_url }
     end
   end
-  
+
   describe "When admin tries to access" do
     before { sign_in admin }
-    
+
     describe "upload page" do
       before { visit new_levelpack_path }
-      
+
       it { should have_content('Create levelpack') }
       it { should have_title(full_title('Create levelpack')) }
-      
+
       describe "and tries to create levelpack" do
         let(:submit) { 'Create levelpack' }
-        
+
         describe "with invalid information" do
           it "should not create a new levelpack" do
             expect { click_button submit }.not_to change Level, :count
-          end          
+          end
         end
-        
+
         describe "with valid information" do
           before do
             fill_in "Name: (format = 'levelpack_00')", with: "levelpack_00"
             fill_in "Title: (max 50 char)",            with: "example title"
           end
-          
+
           it "should create a new levelpack" do
             expect { click_button submit }.to change(Levelpack, :count).by(1)
           end
         end
-      end      
+
+      end
     end
-    
+
     describe "show page" do
       before { visit levelpack_path(levelpack) }
-      
+
       it { should have_content(levelpack.name) }
       it { should have_title(levelpack.name) }
     end
-    
+
+    describe "edit page" do
+      describe "and creates a levelpack relationship" do
+
+        let(:level) { FactoryGirl.create :level }
+
+        before do
+          visit edit_levelpack_path(levelpack)
+          fill_in "Link level:", with: level.name
+          click_button 'Save levelpack'
+        end
+
+        it "should redirect to the show page" do
+          expect(current_path).to eq(levelpack_path levelpack)
+        end
+
+        it "should show up in the levels" do
+          expect(page).to have_content level.name
+        end
+
+      end
+    end
+
     describe "index page" do
-      before do 
+      before do
         visit new_levelpack_path
         fill_in "Name: (format = 'levelpack_00')", with: "levelpack_00"
         fill_in "Title: (max 50 char)",            with: "example title"
         click_button 'Create levelpack'
-        visit levelpacks_path        
+        visit levelpacks_path
       end
-      
+
       it { should have_selector('li', text: "levelpack_00") }
-      it { should have_title 'Levelpack index' }    
-      it { should have_content 'Levelpack index' }  
+      it { should have_title 'Levelpack index' }
+      it { should have_content 'Levelpack index' }
       it { should have_link 'delete' }
-      
+
       describe "and tries to delete an existing levelpack" do
         it "should work" do
           expect do
-            click_link 'delete', match: :first          
+            click_link 'delete', match: :first
           end.to change(Levelpack, :count).by(-1)
         end
       end
