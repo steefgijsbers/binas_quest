cf9b91e673c6bb4e3df35ae7bc78e43d5bc91ace
Steef Gijsbers <steef_gijsbers@hotmail.com>
2014-05-01 13:08:41 +0200

    changed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501081759) do
+ActiveRecord::Schema.define(version: 20140501074125) do
 
   create_table "levelpacks", force: true do |t|
     t.string   "name"
