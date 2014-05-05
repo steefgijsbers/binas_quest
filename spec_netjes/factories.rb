d1beadf9936d17608b8b3aae123bf341d9bf4dbf
Steef Gijsbers <steef_gijsbers@hotmail.com>
2014-05-05 15:39:27 +0200

    Created files and folders for spec_netjes

---
+++ spec_netjes/factories.rb
@@ -0,0 +1,27 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:naam)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    klas "5Hb"
+    progress "start"
+    password "foobar"
+    password_confirmation "foobar"
+         
+    factory :admin do
+      admin true
+    end
+  end
+
+  
+  factory :level do
+    name      "examplelevel"
+    img_src   "examplelevel.jpg"
+    solution  "he"
+  end
+  
+  factory :levelpack do
+    name      "levelpack_00"
+    title     "example title"
+    solution  ""
+  end
+end
